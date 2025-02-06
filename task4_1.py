5156c489d409e9c916588a9c99c26c3c414aeaf4
Volodymyr Pryjmak <v_pryjmak@ukr.net>
2025-02-06 21:31:50 +0200

    Add files via upload

--- task4_1.py
+++ task4_1.py
@@ -1,7 +1,8 @@
 import re
 
 def total_salary(path):
-    sal_list = []
+    total = 0
+    average = 0
     try:
         with open(path, 'r', encoding='utf-16', errors='ignore') as salary:
             # якщо копіюиати з прикладу завдання utf-16, yf, набрати вручну - utf-8
@@ -11,14 +12,12 @@ def total_salary(path):
                 n_row += 1
                 el2 = re.split(',',el.strip())
                 total += int(el2[1])
-             
-            sal_list.append(total)
-            sal_list.append(total/n_row)
-            
-            return sal_list    
+
+            average = total/n_row
     except:
         print("Файл з даними зарплати не знайдено")
-        return None
-sal_list = total_salary("salary_file.txt") 
-if sal_list != None:
-    print(f"Загальна сума {sal_list[0]}  Середня сума {sal_list[1]:.2f}")         
+
+    return total, average
+total, average = total_salary("salary_file.txt") 
+if total > 0:
+   print(f"Загальна сума {total}  Середня сума {average:.2f}")
