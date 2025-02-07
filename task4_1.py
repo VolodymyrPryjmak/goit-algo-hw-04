b2af48019a4b62e6e06e17927f98830caf43fc55
Volodymyr Pryjmak <v_pryjmak@ukr.net>
2025-02-07 22:38:53 +0200

    Add files via upload

--- task4_1.py
+++ task4_1.py
@@ -1,21 +1,23 @@
 import re
 
 def total_salary(path):
-    total = 0
-    average = 0
+    total = 0.0
+    average = 0.0
     try:
         with open(path, 'r', encoding='utf-16', errors='ignore') as salary:
-            # якщо копіюиати з прикладу завдання utf-16, yf, набрати вручну - utf-8
+            # якщо копіюиати з прикладу завдання utf-16,  набрати вручну - utf-
+            # перший раз записав в файл дані з прикладу дом. завдання,
+            # і зчитало utf-16
             n_row = 0
-            total = 0
             for el in salary.readlines():
                 n_row += 1
                 el2 = re.split(',',el.strip())
-                total += int(el2[1])
+                total += float(el2[1])
 
-            average = total/n_row
+            if n_row > 0:
+               average = total/n_row
     except:
-        print("Файл з даними зарплати не знайдено")
+        print(f"Файл з даними зарплати {path} не знайдено")
 
     return total, average
 total, average = total_salary("salary_file.txt") 
